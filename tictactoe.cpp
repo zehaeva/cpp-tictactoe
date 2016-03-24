3df58f52e41cca7eebdd4000a2eefca12e8991be
Howard Canaway <howard.canaway@gmail.com>
2016-03-24 14:05:19 -0400

    Win Conditions
    Added in diagonal win conditions, added breaks so we don't spend time looping over the board when we know we've already won!

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -37,9 +37,16 @@ bool game_won(int board[3][3]) {
 	for (int i=0;i<3;i++) {
 		if (board[i][0] != 0 && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
 			won = true;
+			break;
 		} else if (board[0][i] != 0 && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
 			won = true;
-		}
+			break;
+		} 
+	}
+	if (board[0][0] != 0 && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+		won = true;
+	} else if (board[0][2] != 0 && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+		won = true;
 	}
 
 	return won;
