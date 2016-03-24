19bf8f0d50e271e9d3dcad23ba9abf828671f01b
Howard Canaway <howard.canaway@gmail.com>
2016-03-24 11:16:48 -0400

    Getting Input
    first iteration of getting the x & y coordiantes that you'll play!

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,28 +1,30 @@
 #include "iostream"
 
+using namespace std;
+
 void print_board(int board[3][3]) {
 	for (int i=0;i<3;i++) {
 		for (int j=0;j<3;j++) {
 			switch (board[i][j]) {
 				case 0:
-					std::cout << "_";
+					cout << "_";
 					break;
 				case 1:
-					std::cout << "X";
+					cout << "X";
 					break;
 				case 2:
-					std::cout << "O";
+					cout << "O";
 					break;
 			}
-			std::cout << " ";
+			cout << " ";
 		}
-		std::cout << "\n";
+		cout << "\n";
 	}
 }
 
 int main(void) {
 	int board[3][3];
-	int i, j;
+	int i, j, x, y;
 
 	for (i=0;i<3;i++) {
 		for (j=0;j<3;j++) {
@@ -31,5 +33,12 @@ int main(void) {
 	}
 	
 	print_board(board);
+
+	cout << "Enter the row: ";
+	cin >> x;
+	cout << "Enter the column: ";
+	cin >> y;
+
+	cout << "You entered " << x << ", " << y << "\n";
 }
 
